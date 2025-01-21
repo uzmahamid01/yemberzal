eafe525f66674ffeb8118d5d0d44f6049ddbf072
Uzma Hamid <95444550+uzmahamid01@users.noreply.github.com>
2025-01-21 16:18:34 -0600

    fixed image issue3

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -155,8 +155,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, '../frontend/build/static'),
+    os.path.join(BASE_DIR, '../frontend/build'),
 ]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.views.generic import TemplateView
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,4 +10,4 @@ urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html')),
     # Add this catch-all URL pattern at the end
     re_path(r'^(?!api/).*', TemplateView.as_view(template_name='index.html')),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
