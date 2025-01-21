04142e34e2aa1adfcad71d7ab7796d5cf751b55c
Uzma Hamid <95444550+uzmahamid01@users.noreply.github.com>
2025-01-21 14:43:08 -0600

    production ready

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -6,6 +6,6 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('products.urls')),
-    path("", TemplateView.as_view(template_name="build/index.html")),
+    path("", TemplateView.as_view(template_name="frontend/build/index.html")),
 
 ]
