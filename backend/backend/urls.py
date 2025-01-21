0f31a741c20fd7c82122eb92712139e0e7e98fac
Uzma Hamid <95444550+uzmahamid01@users.noreply.github.com>
2025-01-21 15:40:15 -0600

    check

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -6,7 +6,7 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('products.urls')),
-    path("", TemplateView.as_view(template_name="frontend/build/index.html")),
-    # Catch-all URL pattern for frontend routing
-    re_path(r'^(?!api/).*', TemplateView.as_view(template_name='frontend/build/index.html')),
+    path('', TemplateView.as_view(template_name='index.html')),
+    # Add this catch-all URL pattern at the end
+    re_path(r'^(?!api/).*', TemplateView.as_view(template_name='index.html')),
 ]
