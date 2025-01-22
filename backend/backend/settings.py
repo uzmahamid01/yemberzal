c2df04ccf11bc51a65de8a5ec485fa267e9f79ed
Uzma Hamid <95444550+uzmahamid01@users.noreply.github.com>
2025-01-21 19:15:42 -0600

    test

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -174,11 +174,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #     f"https://{os.getenv('ALLOWED_HOST', 'yemberzal-app-28481d1d0b39.herokuapp.com')}"
 # ]
 
-# CORS_ALLOWED_ORIGINS = [
-#     "http://localhost:3000",  # Frontend during local dev
-#     "https://yemberzal-app-28481d1d0b39.herokuapp.com",  # Frontend on Heroku
-#     "http://127.0.0.1:8000",  # Local backend during dev
-# ]
+CORS_ALLOWED_ORIGINS = [
+    'https://yemberzal-app-28481d1d0b39.herokuapp.com',  # Add your frontend URL here
+]
 
 CORS_ALLOW_METHODS = [
     'GET',
@@ -188,7 +186,9 @@ CORS_ALLOW_METHODS = [
     'PATCH',
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+
 
 import dj_database_url
 
