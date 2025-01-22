9cf4149eae4e211d32d8a1a1a90d89e64af51f0f
Uzma Hamid <95444550+uzmahamid01@users.noreply.github.com>
2025-01-21 18:25:27 -0600

    testt

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -152,7 +152,7 @@ USE_TZ = True
 #     BASE_DIR.parent / 'frontend/build/static',
 # ]
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, '../frontend/build/static'),
