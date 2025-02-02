d4c4d0a83ae73e100cfcc55472b0c3f3b190ab7e
Uzma Hamid <95444550+uzmahamid01@users.noreply.github.com>
2025-02-01 23:01:36 -0600

    added instagram spotlight

---
+++ backend/staticfiles/js/388.2339aa5b.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkfrontend=self.webpackChunkfrontend||[]).push([[388],{388:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=388.2339aa5b.chunk.js.map
